5e6d4024b848f492283afb8f3bb3f6424fd61d7a
aniket <aniketgawali098@gmail.com>
2023-04-27 10:49:50 +0530

    UseState Assignment complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
+
+  
 import logo from './logo.svg';
 import './App.css';
+import { Count } from './Component/count';
 
 function App() {
 
-  //  solition : all the code will have to write here ..
-  //  i have wrotten all the code here only ...
+
+return(
+<div className='count'>
+<Count/>
+</div>
+)
   
-  return  (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
+
